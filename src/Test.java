8881b0b1f3f03bb7366b60ff6773a31d4e66369f
Kasper Weber <kasper@weber-andersen.dk>
2018-11-22 14:00:33 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Test {
 
     public static void main( String[] args){
         System.out.println("Hello World!");
     }
+
+    public static void hey(){
+        System.out.println("hej");
+
+    }
+
 }
